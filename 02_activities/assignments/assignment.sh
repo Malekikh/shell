9dac9721f3505244e5ef00e9fe57f4e23de14daa
Kat <khatere.maleki@gmail.com>
2024-11-23 18:53:44 -0500

    Completed assignement and pushing to assignment branch.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,21 +23,41 @@ unzip rawdata.zip
 # 1. Create a directory named data
 mkdir data
 
-
 # 2. Move the ./rawdata directory to ./data/raw
+ mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir data/processed
+
+mkdir data/processed/server_logs
+mkdir data/processed/user_logs
+mkdir data/processed/event_logs
+
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cp data/raw/*server*.log data/processed/server_logs
+
 
 # 6. Repeat the above step for user logs and event logs
 
+cp data/raw/*user*.log data/processed/user_logs
+cp data/raw/*event*.log data/processed/event_logs
+
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+## -i can be added after rm to see what items are being removed before deleting them. It may not be feasible for a large file but it is a good practice to check what is being removed.
+rm -i data/raw/*ipaddr*
+
+rm data/processed/user_logs/*ipaddr*
+
+
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
+
+$ ls data/processed >> data/inventory.txt
 
 
 ###########################################
