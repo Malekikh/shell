e1569a533e39cba5aa7813c21f45f5c716098081
Kat <khatere.maleki@gmail.com>
2024-11-23 19:06:54 -0500

    Merged conflict is not being correctly reflected on my github

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,16 +48,11 @@ cp data/raw/*user*.log data/processed/user_logs
 cp data/raw/*event*.log data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
 ## -i can be added after rm to see what items are being removed before deleting them. It may not be feasible for a large file but it is a good practice to check what is being removed.
 rm -i data/raw/*ipaddr*
 
 rm data/processed/user_logs/*ipaddr*
 
-
-=======
-rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
